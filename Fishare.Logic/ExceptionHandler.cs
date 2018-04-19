078f6643d74113f2b41752f60a128a93b8e9c26b
DaveyCornelissen <d.cornelissen8@gmail.com>
2018-04-19 09:43:36 +0200

    added a comment to the exceptionHandler

--- Fishare.Logic/ExceptionHandler.cs
+++ Fishare.Logic/ExceptionHandler.cs
@@ -6,8 +6,16 @@ namespace Fishare.Logic
 {
     public class ExceptionHandler : Exception
     {
+        /// <summary>
+        /// This property is used by the ASP.NET Constructor
+        /// </summary>
         public string Index { get; private set; }
 
+        /// <summary>
+        /// This constructure is for APS.NET ViewData[]
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="message"></param>
         public ExceptionHandler(string index, string message)
             : base(message)
         {
